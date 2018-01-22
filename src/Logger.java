c5bb6838384cae9ac1f74243af09d0da6aed6cd6
Tamas Kiss <sauriaster@gmail.com>
2018-01-22 15:31:34 +0100

    added static counter

--- src/Logger.java
+++ src/Logger.java
@@ -2,10 +2,13 @@ package src;
 import java.util.Date;
 
 public class Logger {
+    static int counter = 1;
+
     public void log(String type, String message){
         Date now = new Date();
         now.getTime();
-        System.out.format("\n%s %s %s\n", now, type, message);
+        System.out.format("\n%d.  %s %s %s\n",counter, now, type, message);
+        counter++;
         
     }
 }
