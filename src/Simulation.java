92a10f58819a286a48273cac8c51c0a474725886
Gyorfisti <gyorfisti@gmail.com>
2018-01-22 14:05:38 +0100

    added getter to id in Simulation

--- src/Simulation.java
+++ src/Simulation.java
@@ -5,20 +5,13 @@ public class Simulation {
 
     private int id;
     private Result result;
-    private int rndInt;
 
     public Simulation(int round) {
         id = round;
     }
 
-    public int testRandom(){
-        Random rnd = new Random();
-        rndInt = rnd.nextInt(50);
-        return rndInt;
-    }
-
-    public int load(){
-        return rndInt;
+    public int getId() {
+        return id;
     }
 
     public void setResult(Result result) {
