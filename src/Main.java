1d612ac7a025c71840cd2a705bdcf4538b2d6191
Tamas Kiss <sauriaster@gmail.com>
2018-01-22 11:19:49 +0100

    added some method for sketching

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 package src;
 
 public class Main {
+
+public static Simulation generateSimulation(int round){
+    Simulation simulation = new Simulation();
+    // for( int i = 0; i < round; i++){} // run simulation by times
+    return simulation;    
+}
+
+
     
+public static void main(String[] args){
+    Main.generateSimulation(Integer.parseInt(args[0]));
+    }
+
 }
