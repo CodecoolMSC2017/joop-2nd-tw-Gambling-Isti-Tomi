9f88f1a35eedefd6d806d9a191ae19a58c825b90
Tamas Kiss <sauriaster@gmail.com>
2018-01-22 14:03:26 +0100

    initialized Cock

---
+++ src/Cock.java
@@ -0,0 +1,13 @@
+public class Cock{
+    String name;
+    int speed;
+    int weigth;
+    int height;
+
+    public Cock(String name, int speed, int weigth, int height){
+        this.name = name;
+        this.speed = speed;
+        this.weigth = weigth;
+        this.height = height;
+    }
+}
