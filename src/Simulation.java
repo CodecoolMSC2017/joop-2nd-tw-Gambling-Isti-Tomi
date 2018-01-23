bb66f41e5030b19617cbe74d0cc757dda53aeea1
Tamas Kiss <sauriaster@gmail.com>
2018-01-23 11:31:08 +0100

    added generateData

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,8 +1,8 @@
 package src;
 import java.util.Random;
+import java.io.*;
 
 public class Simulation {
-
     private int id;
     private Cock[] cocks;
     private Result result;
@@ -10,6 +10,14 @@ public class Simulation {
     public Simulation(int round, Cock[] cocks) {
         this.id = round;
         this.cocks = cocks;
+    }
+
+    public void generateData(String filePath){
+        File file = new File(filePath);
+        FileWriter fileWriter = new FileWriter(file,true);
+        BufferedWriter bufferFileWriter  = new BufferedWriter(fileWriter);
+        fileWriter.append(result.toSting());
+        file.close();
     }
 
     public int getId() {
