73418f6f0821c56cca0e50cfd3d33104cb668423
Tamas Kiss <sauriaster@gmail.com>
2018-01-23 14:54:54 +0100

    added load(), compiles but not tested

--- src/Cock.java
+++ src/Cock.java
@@ -16,4 +16,20 @@ public class Cock{
     public int getTotalPoints() {
         return speed * 2 + weight + height;
     }
+
+    @Override
+    public boolean equals(Object obj){
+        if(obj == this){
+            return true;
+        }
+        if (!(obj instanceof Cock) || obj == null){
+            return false;
+        }
+        Cock temp = (Cock)obj;
+        if (this.name.equals(temp.name) && this.speed == temp.speed &&
+            this.weight == temp.weight &&  this.height == temp.height) {
+            return true;
+        }
+        return false;
+    }
 }
