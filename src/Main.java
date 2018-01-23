1d770a9a1e2b0aee8df19ef63be9961bbef6afeb
Tamas Kiss <sauriaster@gmail.com>
2018-01-23 12:52:53 +0100

    added more cock to the party

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,7 @@ public class Main {
     }
 
     public static void main(String[] args){
-        int amountOfRounds = 0;
-        try {
-            amountOfRounds = Integer.parseInt(args[0]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Usage: java src.Main [amount of simulations]\n" +
-                "Note: number cannot be bigger than 1 000 000");
-            System.exit(-1);
-        }
-        if (amountOfRounds > 1000000) {
-            System.out.println("Number cannot be bigger than 1 000 000");
-            System.exit(-1);
-        }
+        int amountOfRounds = Integer.parseInt(args[0]);
         Simulation[] simulations = new Simulation[amountOfRounds];
         Cock[] cocks = generateCocks();
         for (int round = 0; round < amountOfRounds; round++) {
@@ -58,13 +47,16 @@ public class Main {
     }
 
     private static Cock[] generateCocks() {
-        final int amount = 5;
+        final int amount = 8;
         Cock[] cocks = new Cock[amount];
-        cocks[0] = new Cock("Mr. Sniffles", 2, 5, 37);
-        cocks[1] = new Cock("Womb Raider", 1, 5, 38);
-        cocks[2] = new Cock("Wedding Wrecker", 4, 4, 34);
+        cocks[0] = new Cock("Mr. Sniffles", 2, 5, 34);
+        cocks[1] = new Cock("Womb Raider", 1, 5, 37);
+        cocks[2] = new Cock("Wedding Wrecker", 4, 4, 33);
         cocks[3] = new Cock("Clam Hammer", 3, 4, 35);
-        cocks[4] = new Cock("Muff Marauder", 5, 3, 33);
+        cocks[4] = new Cock("Muff Marauder", 1, 1, 38);
+        cocks[5] = new Cock("AIDS Baster", 5, 5, 24);
+        cocks[6] = new Cock("Kidney Scraper", 3, 3, 30);
+        cocks[7] = new Cock("Ho Wrecker", 1, 2, 35);
         return cocks;
     }
 
